7885af03c24a5a7e8d7371ff4c4be314bc378235
ngaibr22 <157721624+ngaibr22@users.noreply.github.com>
2025-02-25 17:26:05 -0500

    Update prob3.v

--- src/prob3.v
+++ src/prob3.v
@@ -15,16 +15,18 @@ module tt_um_BRS_3 (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-    // Conditional bitwise XOR/AND logic
-    assign uo_out[0] = (ui_in[7] == 0) ? (ui_in[0] ^ uio_in[0]) : (ui_in[0] & uio_in[0]);
-    assign uo_out[1] = (ui_in[7] == 0) ? (ui_in[1] ^ uio_in[1]) : (ui_in[1] & uio_in[1]);
-    assign uo_out[2] = (ui_in[7] == 0) ? (ui_in[2] ^ uio_in[2]) : (ui_in[2] & uio_in[2]);
-    assign uo_out[3] = (ui_in[7] == 0) ? (ui_in[3] ^ uio_in[3]) : (ui_in[3] & uio_in[3]);
-    assign uo_out[4] = (ui_in[7] == 0) ? (ui_in[4] ^ uio_in[4]) : (ui_in[4] & uio_in[4]);
-    assign uo_out[5] = (ui_in[7] == 0) ? (ui_in[5] ^ uio_in[5]) : (ui_in[5] & uio_in[5]);
-    assign uo_out[6] = (ui_in[7] == 0) ? (ui_in[6] ^ uio_in[6]) : (ui_in[6] & uio_in[6]);
-    assign uo_out[7] = (ui_in[7] == 0) ? (ui_in[7] ^ uio_in[7]) : (ui_in[7] & uio_in[7]);
+    reg [7:0] C;
+    assign uo_out = C;
+    always @* begin
+        
+    if (ui_in < uio_in) begin
+        C = uio_in;
+    end
+        else begin
+            C = ui_in;
+            endcase
+        end
+    end
     
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
