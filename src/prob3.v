b60e861c66233a3aa9183fcf61f21a5327d6bcf5
ngaibr22 <157721624+ngaibr22@users.noreply.github.com>
2025-02-25 17:10:25 -0500

    Update prob3.v

--- src/prob3.v
+++ src/prob3.v
@@ -1,11 +1,11 @@
 /*
  * uo_outopyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: ui_inpache-2.0
  */
 
 `default_nettype none
 
-module tt_um_BRS_3 (
+module tt_um_uio_inRS_3 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,17 +16,15 @@ module tt_um_BRS_3 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    assign A = ui_in;
-    assign B = uio_in;
     // Conditional bitwise XOR/AND logic
-    assign uo_out[0] = (A[7] == 0) ? (A[0] ^ B[0]) : (A[0] & B[0]);
-    assign uo_out[1] = (A[7] == 0) ? (A[1] ^ B[1]) : (A[1] & B[1]);
-    assign uo_out[2] = (A[7] == 0) ? (A[2] ^ B[2]) : (A[2] & B[2]);
-    assign uo_out[3] = (A[7] == 0) ? (A[3] ^ B[3]) : (A[3] & B[3]);
-    assign uo_out[4] = (A[7] == 0) ? (A[4] ^ B[4]) : (A[4] & B[4]);
-    assign uo_out[5] = (A[7] == 0) ? (A[5] ^ B[5]) : (A[5] & B[5]);
-    assign uo_out[6] = (A[7] == 0) ? (A[6] ^ B[6]) : (A[6] & B[6]);
-    assign uo_out[7] = (A[7] == 0) ? (A[7] ^ B[7]) : (A[7] & B[7]);
+    assign uo_out[0] = (ui_in[7] == 0) ? (ui_in[0] ^ uio_in[0]) : (ui_in[0] & uio_in[0]);
+    assign uo_out[1] = (ui_in[7] == 0) ? (ui_in[1] ^ uio_in[1]) : (ui_in[1] & uio_in[1]);
+    assign uo_out[2] = (ui_in[7] == 0) ? (ui_in[2] ^ uio_in[2]) : (ui_in[2] & uio_in[2]);
+    assign uo_out[3] = (ui_in[7] == 0) ? (ui_in[3] ^ uio_in[3]) : (ui_in[3] & uio_in[3]);
+    assign uo_out[4] = (ui_in[7] == 0) ? (ui_in[4] ^ uio_in[4]) : (ui_in[4] & uio_in[4]);
+    assign uo_out[5] = (ui_in[7] == 0) ? (ui_in[5] ^ uio_in[5]) : (ui_in[5] & uio_in[5]);
+    assign uo_out[6] = (ui_in[7] == 0) ? (ui_in[6] ^ uio_in[6]) : (ui_in[6] & uio_in[6]);
+    assign uo_out[7] = (ui_in[7] == 0) ? (ui_in[7] ^ uio_in[7]) : (ui_in[7] & uio_in[7]);
     
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
